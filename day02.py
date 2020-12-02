f015bafc4ded3e0ab9b0bc30be2f10289111c1d3
Olfa Karoui <36195894+OlfaKaroui@users.noreply.github.com>
2020-12-02 20:07:44 +0100

    day 2  🎉

---
+++ day02.py
@@ -0,0 +1,50 @@
+import re
+from data_day02 import *
+
+def getIfMatchesNumber(input):
+    minimum, maximum, character, password = extractRule(input)
+    count = password.count(character)
+    return count in range(int(minimum), int(maximum) + 1)
+
+
+def getIfMatchesPosition(input, number):
+    minimum, maximum, character, password = extractRule(input)
+    firstCharMatches = password[int(minimum) -1] == character
+    secondCharMatches = password[int(maximum) -1] == character
+    return secondCharMatches is not firstCharMatches
+
+
+def extractRule(input):
+    pattern = re.compile(r"^(?P<min>.\d?)+\-(?P<max>.\d?)+\s+(?P<character>.[a-zA-Z]?)+\:\s+(?P<password>.*?)$", re.VERBOSE)
+    match = pattern.match(input)
+
+    return (match.group("min"), match.group("max"),match.group("character"), match.group("password"))
+
+
+number = 0
+numberPositions = 0
+for input in inputs : 
+    if getIfMatchesNumber(input):
+        number+= 1
+    if getIfMatchesPosition(input,numberPositions):
+        numberPositions+= 1
+
+print number
+print numberPositions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
